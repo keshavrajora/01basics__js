d1ef4e97f10a1877b5e8ebec111591018a45b23e
Keshav Jangid 009 <149224434+keshavrajora@users.noreply.github.com>
2024-08-31 22:43:45 +0530

    Add files via upload

---
+++ 05_iterations/five.js
@@ -0,0 +1,61 @@
+    const codeing = ["js", "cpp", "rb", "swift", "python",];
+
+    // codeing.forEach(function (val){
+    //     console.log(val);
+        
+    // })
+
+// codeing.forEach(value => {
+//     console.log(value);
+    
+// });
+
+
+// codeing.forEach ( (val) => {
+//     console.log(val);
+    
+// })
+
+
+// function printMe(val){
+//     console.log(val);
+    
+// }
+// codeing.forEach(printMe)
+
+
+// codeing.forEach((key, index, arr) => {
+    //     console.log(key, index, arr);
+        
+        
+    // });
+
+const myCodeing = [
+    {
+        languageName: "javascript",
+        languageFileName: "js",
+    },
+    {
+        languageName: "java",
+        languageFileName: "java",
+    },
+    {
+        languageName: "cpp",
+        languageFileName: "c++",
+    },
+    {
+        languageName: "python",
+        languageFileName: "py",
+    },
+    {
+        languageName: "typescript",
+        languageFileName: "ts",
+    },
+]
+
+myCodeing.forEach((itme) => {
+    console.log(itme.languageName);
+    
+    
+    
+})
